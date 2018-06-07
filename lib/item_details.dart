a20c8330c8facfd1854162e399cd02205d26d069
MartoAndAlex <martoandalex@Ufsa-MacBook-Pro.local>
2018-06-07 13:18:56 +0300

    init

---
+++ lib/item_details.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_master_detail/item.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class ItemDetails extends StatelessWidget {
+  ItemDetails({
+    @required this.isInTabletLayout,
+    @required this.item,
+  });
+
+  final bool isInTabletLayout;
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final Widget content = new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        new Text(
+          item?.title ?? 'No item selected!',
+          style: textTheme.headline,
+        ),
+        new Text(
+          item?.subtitle ?? 'Please select one on the left.',
+          style: textTheme.subhead,
+        ),
+      ],
+    );
+
+    if (isInTabletLayout) {
+      return new Center(child: content);
+    }
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(item.title),
+      ),
+      body: new Center(child: content),
+    );
+  }
+}
---
+++ lib/item_listing.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_master_detail/item.dart';
+import 'package:flutter_master_detail/item_details.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class ItemListing extends StatelessWidget {
+  ItemListing({
+    @required this.itemSelectedCallback,
+    this.selectedItem,
+  });
+
+  final ValueChanged<Item> itemSelectedCallback;
+  final Item selectedItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      children: items.map((item) {
+        return new ListTile(
+          title: new Text(item.title),
+          onTap: () => itemSelectedCallback(item),
+          selected: selectedItem == item,
+        );
+      }).toList(),
+    );
+  }
+}
