cff7fb248cda533f2af8b64501aa53acf6aa0229
MartoAndAlex <martoandalex@Ufsa-MacBook-Pro.local>
2018-06-08 11:19:43 +0300

    style details

--- lib/item.dart
+++ lib/item.dart
@@ -4,63 +4,63 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Item {
-  Item({
-    @required this.doors,
-    @required this.hasAC,
-    @required this.id,
-    @required this.imageStoragePath,
-    @required this.imageUrl,
-    @required this.luggage,
-    @required this.name,
-    @required this.price,
-    @required this.seats,
-    @required this.transmission
-  });
+  Item(
+      {@required this.doors,
+      @required this.hasAC,
+      @required this.id,
+      @required this.classType,
+      @required this.imageStoragePath,
+      @required this.imageUrl,
+      @required this.luggage,
+      @required this.name,
+      @required this.price,
+      @required this.seats,
+      @required this.transmission});
 
-    final int doors;
-    final bool hasAC;
-    final String id;
-    final String imageStoragePath;
-    final String imageUrl;
-    final int luggage;
-    final String name;
-    final int price;
-    final String seats;
-    final String transmission;
+  final int doors;
+  final bool hasAC;
+  final String classType;
+  final String id;
+  final String imageStoragePath;
+  final String imageUrl;
+  final int luggage;
+  final String name;
+  final int price;
+  final String seats;
+  final String transmission;
 
-    factory Item.fromJson(Map<String, dynamic> json) {
+  factory Item.fromJson(Map<String, dynamic> json) {
     return new Item(
-      doors: json['doors'],
-      hasAC: json['hasAC'],
-      id: json['id'],
-      imageStoragePath: json['imageStoragePath'],
-      imageUrl: json['imageUrl'],
-      luggage:json['luggage'],
-      name: json['name'],
-      price: json['price'],
-      seats: json['seats'],
-      transmission:json['transmission']
-    );
+        doors: json['doors'],
+        hasAC: json['hasAC'],
+        classType: json['class'],
+        id: json['id'],
+        imageStoragePath: json['imageStoragePath'],
+        imageUrl: json['imageUrl'],
+        luggage: json['luggage'],
+        name: json['name'],
+        price: json['price'],
+        seats: json['seats'],
+        transmission: json['transmission']);
   }
 }
 
 Future<List<Item>> fetchItem() async {
-
-  if(items.length>0) {
+  if (items.length > 0) {
     return items;
   }
 
   final List<Item> tempItems = <Item>[];
 
-  final response =
-    await http.get('https://gist.githubusercontent.com/ADjenkov/d524f9736341f4ca3354ccfaacb72477/raw/e51659f0c95e84a2d5a65354aeffa318e496eb39/cars-son');
+  final response = await http.get(
+      'https://gist.githubusercontent.com/ADjenkov/d524f9736341f4ca3354ccfaacb72477/raw/e51659f0c95e84a2d5a65354aeffa318e496eb39/cars-son');
   final responseJson = json.decode(response.body);
 
   for (Map<String, dynamic> car in responseJson["cars"]) {
-      tempItems.add(new Item.fromJson(car));
+    tempItems.add(new Item.fromJson(car));
   }
 
   return tempItems;
 }
 
- List<Item> items = <Item>[];
+List<Item> items = <Item>[];
--- lib/item_details.dart
+++ lib/item_details.dart
@@ -14,62 +14,125 @@ class ItemDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
-    final Widget content =
-        new Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      new Image.network(item.imageUrl),
-      new Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+    final Widget content = new Container(
+        decoration: new BoxDecoration(color: Colors.white),
+        child:
+            new Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+         new AspectRatio(aspectRatio: 16/9, child: 
+        new Container(height: 200.0,child: 
+          new Image.network(item.imageUrl, fit: BoxFit.fitWidth)),
+         ),
+          new Divider(),
+          new Column(
             children: [
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
+              new Row(
                 children: [
-                  new Text(
-                    "Price",
-                    textAlign: TextAlign.left,
-                    style: textTheme.subhead,
-                  ),
-                  new Text(
-                    "Class",
-                    style: textTheme.subhead,
-                  ),
-                  new Text(
-                    "Doors",
-                    style: textTheme.subhead,
-                  ),
-                  new Text(
-                    "Seats",
-                    style: textTheme.subhead,
-                  ),
-                  new Text(
-                    "Transmission",
-                    style: textTheme.subhead,
-                  ),
-                  new Text(
-                    "Luggage",
-                    style: textTheme.subhead,
-                  ),
+                  new Padding(
+                      padding: new EdgeInsets.only(left: 10.0),
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Price",
+                                textAlign: TextAlign.left,
+                               style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Class",
+                                style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Doors",
+                                style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Seats",
+                               style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Transmission",
+                               style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                "Luggage",
+                              style: new TextStyle(color: Colors.grey, fontSize: 16.0)
+                              )),
+                        ],
+                      )),
+                  new Padding(
+                      padding: new EdgeInsets.only(left: 50.0),
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.price.toString(),
+                                style: textTheme.subhead,
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.classType.toString(),
+                                style: textTheme.subhead,
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.doors.toString(),
+                                style: textTheme.subhead,
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.seats,
+                                style: textTheme.subhead,
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.transmission,
+                                style: textTheme.subhead,
+                              )),
+                          new Container(
+                              margin:
+                                  new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                              child: new Text(
+                                item.luggage.toString(),
+                                style: textTheme.subhead,
+                              ))
+                        ],
+                      ))
                 ],
               ),
-              new Padding(
-                  padding: new EdgeInsets.only(left: 50.0),
-                  child: new Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      new Text(
-                        item.price.toString(),
-                        style: textTheme.subhead,
-                      ),
-                    ],
-                  ))
             ],
-          ),
-        ],
-      )
-    ]);
+          )
+        ]));
 
     if (isInTabletLayout) {
       return new Center(child: content);
